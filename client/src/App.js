483bee0bc8e38bb7e261a15139365bcf50958341
Patrick Klima <patrick.klima@gmail.com>
2018-01-24 18:09:55 +0000

    React skeleton done. Client and server will run.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Slider from 'react-rangeslider'
+import BootstrapTable from 'reactjs-bootstrap-table';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container-fluid">
+        <div className="row show-border">
+          <div className="col">
+            <BootstrapTable columns={0} data={0} headers={true}>
+              <div className="well">There are no items to show</div>
+            </BootstrapTable>
+          </div>
+          <div className="col">
+            <div className="row show-border">
+              <div className="col">
+                <Slider
+                  min={0}
+                  max={100}
+                  step={1}
+                  value={50}
+                  orientation={"horizontal"}
+                />        
+              </div>
+            </div>
+            <div className="row show-border">
+              <div className="col">
+                <BootstrapTable columns={0} data={0} headers={true}>
+                  <div className="well">There are no items to show</div>
+                </BootstrapTable>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
